1efaab64cd9764b32bdd10c92d4496468e797688
justinsiek <justinsiek05@gmail.com>
2025-11-28 10:59:23 -0800

    works on phone

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,13 +1,12 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
-import './global.css';
-
+import "./global.css"
+import { Text, View } from "react-native";
+ 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-700">
+        Hello World
+      </Text>
+    </View>
   );
 }
